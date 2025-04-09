087adbbb08f874cc7b7578144fc51e8a5ae35701
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-09 12:10:14 -0700

    Update intro.tsx

--- components/home-page/intro.tsx
+++ components/home-page/intro.tsx
@@ -3,8 +3,7 @@ import { Twemoji } from '~/components/ui/twemoji'
 export function Intro() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
-      I'm <span className="font-medium">David Galiata</span> - I'm passionate about cloud, security, and 
-      <span className="font-medium">DevOps</span>.
+      I'm <span className="font-medium">David Galiata</span> - I'm passionate about cloud, security, and DevOps.
       <span className="absolute ml-1.5 inline-flex pt-[3px]">
       </span>
     </h1>

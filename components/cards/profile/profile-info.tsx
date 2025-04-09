70e97a2ae9ca5fd30ae56f0aa72407979579053d
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-09 12:02:57 -0700

    index homepage updates

--- components/cards/profile/profile-info.tsx
+++ components/cards/profile/profile-info.tsx
@@ -52,7 +52,7 @@ export function ProfileCardInfo() {
         <div className="flex items-center text-gray-700 dark:text-gray-200">
           <MapPin strokeWidth={1.5} size={20} />
           <p className="px-2">
-            [::1]:443 - Petaluma,
+            [::1]:443 - Petaluma,CA
             <span className="absolute ml-1 inline-flex pt-px">
               <Twemoji emoji="flag-us" />
             </span>

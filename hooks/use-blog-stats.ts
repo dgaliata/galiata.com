ee3f4f9c3cbc611137884c6030a9da862015de92
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-09 09:56:01 -0700

    Update use-blog-stats.ts

--- hooks/use-blog-stats.ts
+++ hooks/use-blog-stats.ts
@@ -1,16 +1,25 @@
 import useSWR from 'swr'
 import useSWRMutation from 'swr/mutation'
-import type { SelectStats, StatsType } from '~/db/schema'
 import { fetcher } from '~/utils/misc'
 
-export function useBlogStats(type: StatsType, slug: string) {
-  let { data, isLoading } = useSWR<SelectStats>(`/api/stats?slug=${slug}&type=${type}`, fetcher, {
+type Stats = {
+  type: string; 
+  slug: string;
+  views: number;
+  loves: number;
+  applauses: number;
+  ideas: number;
+  bullseyes: number;
+}
+
+export function useBlogStats(type: string, slug: string) {
+  let { data, isLoading } = useSWR<Stats>(`/api/stats?slug=${slug}&type=${type}`, fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   })
   let { views, loves, applauses, ideas, bullseyes } = data || {}
-  let stats: SelectStats = {
+  let stats: Stats = {
     type,
     slug,
     views: views || 0,
@@ -25,7 +34,7 @@ export function useBlogStats(type: StatsType, slug: string) {
 export function useUpdateBlogStats() {
   let { trigger } = useSWRMutation(
     '/api/stats',
-    async (url: string, { arg }: { arg: Partial<SelectStats> }) => {
+    async (url: string, { arg }: { arg: Partial<Stats> }) => {
       return fetch(url, {
         method: 'POST',
         body: JSON.stringify(arg),

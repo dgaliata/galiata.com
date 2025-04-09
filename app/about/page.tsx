57179f6cb63ff326dc1dd66945e043333721caef
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-09 14:34:35 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,25 +1,14 @@
-import { genPageMetadata } from 'app/seo'
-import type { Author } from 'contentlayer/generated'
-import { allAuthors, allBlogs } from 'contentlayer/generated'
+import { allBlogs } from 'contentlayer/generated'
 import { Home } from '~/components/home-page'
-import { coreContent } from '~/utils/contentlayer'
-
-export let metadata = genPageMetadata({ title: 'Home' })
+import { allCoreContent } from '~/utils/contentlayer'
+import { sortPosts } from '~/utils/misc'
 
 const MAX_POSTS_DISPLAY = 5
 
 export default function HomePage() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Author
-  const mainContent = coreContent(author)
-
-  // Update the line below to use `coreContent` instead of `allCoreContent`
-  const posts = coreContent(sortPosts(allBlogs)).slice(0, MAX_POSTS_DISPLAY)
-
   return (
-    <div>
-      <Home
-        posts={posts}
-      />
-    </div>
+    <Home
+      posts={allCoreContent(sortPosts(allBlogs)).slice(0, MAX_POSTS_DISPLAY)}
+    />
   )
 }

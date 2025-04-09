8fa5b29d3e1ba3e44ce5896b8e99e19259382df3
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-09 14:31:04 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,12 +11,14 @@ const MAX_POSTS_DISPLAY = 5
 export default function HomePage() {
   const author = allAuthors.find((p) => p.slug === 'default') as Author
   const mainContent = coreContent(author)
-  const posts = allCoreContent(sortPosts(allBlogs)).slice(0, MAX_POSTS_DISPLAY)
+
+  // Update the line below to use `coreContent` instead of `allCoreContent`
+  const posts = coreContent(sortPosts(allBlogs)).slice(0, MAX_POSTS_DISPLAY)
 
   return (
     <div>
       <Home
-        posts={posts} // Only passing posts, removing snippets
+        posts={posts}
       />
     </div>
   )

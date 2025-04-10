d4cd99bf7a4e37c3432792a958179289252d8dcc
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-10 11:40:17 -0700

    umami updates

--- components/analytics/umami.tsx
+++ components/analytics/umami.tsx
@@ -1,13 +1,19 @@
-import Script from 'next/script.js'
+import Script from 'next/script'
 
 interface UmamiAnalyticsProps {
   websiteId?: string
   src?: string
 }
 
-export function UmamiAnalytics({ websiteId, src = 'https://cloud.umami.is/script.js' }: UmamiAnalyticsProps) {
-  if (websiteId) {
-    return <Script async defer data-website-id={websiteId} src={src} />
-  }
-  return null
+export function UmamiAnalytics({ 
+  websiteId = "b69c27ce-feac-45db-a6ce-ab7da3c62126",
+  src = 'https://cloud.umami.is/script.js' 
+}: UmamiAnalyticsProps) {
+  return (
+    <Script 
+      src={src}
+      data-website-id={websiteId}
+      strategy="afterInteractive"
+    />
+  )
 }

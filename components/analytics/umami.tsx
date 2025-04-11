e381150472ef04cbe87053ab4342f781ecc4f622
David Galiata | neoslash.net <david.galiata@aquia.io>
2025-04-11 01:06:28 -0700

    Update umami.tsx

--- components/analytics/umami.tsx
+++ components/analytics/umami.tsx
@@ -1,16 +1,10 @@
 import Script from 'next/script'
 import { SITE_METADATA } from '~/data/site-metadata'
 
-// ... (interface definition)
-
 export function UmamiAnalytics({ /* ...props... */ }) {
   const websiteId = SITE_METADATA.analytics.umamiAnalytics.websiteId;
   const src = 'https://cloud.umami.is/script.js';
   const domains = SITE_METADATA.siteUrl.replace(/(^\w+:|^)\/\//, '');
-
-  // --- TEMPORARY LOGGING ---
-  console.log('Rendering Umami Analytics with:', { websiteId, src, domains });
-  // --- END TEMPORARY LOGGING ---
 
   if (!websiteId) {
     console.log('Umami Analytics: No websiteId found, skipping render.'); // Add log here too
